2ca978df30138704ebb2e54309f36166f2551621
Konstantin7553 <krasik150@gmail.com>
2024-11-03 14:57:43 +0300

    Add files via upload

--- module_13_3.py
+++ module_13_3.py
@@ -3,26 +3,20 @@ import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
 
-# Включаем логирование
 logging.basicConfig(level=logging.INFO)
 
-# Инициализируем бот и диспетчер
 bot = Bot(token="")
 dp = Dispatcher()
 
-# Хэндлер на команду /start
-@dp.message(Command("start"))
+@dp.message_handler(commands=["start"])
 async def start(message: types.Message):
     await message.answer("Привет! Я бот помогающий твоему здоровью.")
 
-# Хэндлер на все остальные сообщения
 @dp.message()
 async def all_messages(message: types.Message):
     await message.answer("Введите команду /start, чтобы начать общение.")
 
-# Главная функция
 async def main():
-    # Запускаем бота
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
